652601c6fc30602f15e730e766560db264c8d86d
Emmet Susslin <esusslin@gmail.com>
2017-10-09 12:41:28 -0700

    header component added

--- albums/index.ios.js
+++ albums/index.ios.js
@@ -1,53 +1,14 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
+// Index.ios.js - code for IOS goes here!
 
-import React, { Component } from 'react';
-import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+// Import a library to help create a Component
+import React from 'react';
+import { AppRegistry } from 'react-native';
+import Header from './src/components/header';
 
-export default class albums extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to Reacadsfasdfsadft Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
-    );
-  }
-}
+// Create a component
+const App = () => (
+    <Header headerText={'Albums'} />
+  );
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
-
-AppRegistry.registerComponent('albums', () => albums);
+// Render the component to a device
+AppRegistry.registerComponent('albums', () => App);
