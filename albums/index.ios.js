4008465bc1b75c946eec0c6aa8ff444c589d7f1d
Emmet Susslin <esusslin@gmail.com>
2017-10-09 17:14:00 -0700

    Taylor swift album done

--- albums/index.ios.js
+++ albums/index.ios.js
@@ -1,13 +1,15 @@
-// Index.ios.js - code for IOS goes here!
-
 // Import a library to help create a Component
 import React from 'react';
-import { AppRegistry } from 'react-native';
+import { AppRegistry, View } from 'react-native';
 import Header from './src/components/header';
+import AlbumList from './src/components/AlbumList';
 
 // Create a component
 const App = () => (
+  <View style={{ flex: 1 }}>
     <Header headerText={'Albums'} />
+    <AlbumList />
+  </View>
   );
 
 // Render the component to a device
