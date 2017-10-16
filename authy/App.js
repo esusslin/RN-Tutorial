05150380c357e0bd16b4bb18dbf516aa0b52d735
Emmet Susslin <esusslin@gmail.com>
2017-10-16 07:44:34 -0700

    thru Redux part1

--- authy/App.js
+++ authy/App.js
@@ -8,16 +8,26 @@ class App extends Component {
   state = { loggedIn: null };
 
   componentWillMount() {
+    // firebase.initializeApp({
+    //   apiKey: 'AIzaSyBtxMY4K6uHxv_2e3GD-FWAD2ACX6lPVRE',
+    //   authDomain: 'authentication-70a18.firebaseapp.com',
+    //   databaseURL: 'https://authentication-70a18.firebaseio.com',
+    //   storageBucket: 'authentication-70a18.appspot.com',
+    //   messagingSenderId: '682333809338'
+    // });
+
     firebase.initializeApp({
-      apiKey: 'AIzaSyBtxMY4K6uHxv_2e3GD-FWAD2ACX6lPVRE',
-      authDomain: 'authentication-70a18.firebaseapp.com',
-      databaseURL: 'https://authentication-70a18.firebaseio.com',
-      storageBucket: 'authentication-70a18.appspot.com',
-      messagingSenderId: '682333809338'
-    });
+    apiKey: 'AIzaSyDYfU8PD1yhkKifx8awklxQAXDs7dUcC28',
+    authDomain: 'authy-37cd9.firebaseapp.com',
+    databaseURL: 'https://authy-37cd9.firebaseio.com',
+    projectId: 'authy-37cd9',
+    storageBucket: 'authy-37cd9.appspot.com',
+    messagingSenderId: '337441326411'
+  });
 
     firebase.auth().onAuthStateChanged((user) => {
       if (user) {
+        console.log(user.email);
         this.setState({ loggedIn: true });
       } else {
         this.setState({ loggedIn: false });
